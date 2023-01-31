e262872384ec871e46108dc461d4a8a765bdd957
Noire <noireflynn@gmail.com>
2023-01-31 22:24:47 +0400

    added few code

--- src/test/java/com/demoqa/HomeWork.java
+++ src/test/java/com/demoqa/HomeWork.java
@@ -11,21 +11,32 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class HomeWork {
     @BeforeAll
-static void setUp() {
+    static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
 
     }
+
     @Test
     void fillFormTest() {
-open("/automation-practice-form");
-$("[id=firstName]").setValue("Vlad");
-$("[id=lastName]").setValue("Dolgodvorov");
-$("[id=userEmail]").setValue("test@test.com");
-$(byText("Male")).click();
-$("[id=userNumber]").setValue("9980000000");
-$("[id=dateOfBirthInput]").click();
-$(".react-datepicker__month-select").selectOption("June");
+        open("/automation-practice-form");
+        $("[id=firstName]").setValue("Vlad");
+        $("[id=lastName]").setValue("Dolgodvorov");
+        $("[id=userEmail]").setValue("test@test.com");
+        $(byText("Male")).click();
+        $("[id=userNumber]").setValue("9980000000");
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__day--023").click();
+        $("[id=subjectsInput]").setValue("English").pressEnter();
+        $(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("qa_logo.jpg");
+        $("#currentAddress").setValue("Value");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("[id=submit").click();
     }
+
 }
