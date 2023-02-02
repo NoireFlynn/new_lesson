9fb8468cced9945351005700469c631d68fd8d6f
DolVA <noireflynn@gmail.com>
2023-02-02 19:23:27 +0400

    added few code

--- src/test/java/com/demoqa/HomeWork.java
+++ src/test/java/com/demoqa/HomeWork.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -21,22 +22,33 @@ public class HomeWork {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Vlad");
-        $("[id=lastName]").setValue("Dolgodvorov");
-        $("[id=userEmail]").setValue("test@test.com");
+        $("#firstName").setValue("Vlad");
+        $("#lastName").setValue("Dolgodvorov");
+        $("#userEmail").setValue("test@test.com");
         $(byText("Male")).click();
-        $("[id=userNumber]").setValue("9980000000");
-        $("[id=dateOfBirthInput]").click();
+        $("#userNumber").setValue("9980000000");
+        $("#dateOfBirthInput]").click();
         $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__year-select").selectOption("1995");
         $(".react-datepicker__day--023").click();
-        $("[id=subjectsInput]").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
         $(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("qa_logo.jpg");
         $("#currentAddress").setValue("Value");
         $("#react-select-3-input").setValue("Haryana").pressEnter();
         $("#react-select-4-input").setValue("Karnal").pressEnter();
-        $("[id=submit").click();
+        $("#submit").click();
+
+        $(".table-responsive").$(byText("Student Name")).parent().lastChild().shouldHave(text("Vlad Dolgodvorov"));
+        $(".table-responsive").$(byText("Student Email")).parent().lastChild().shouldHave(text("test@test.com"));
+        $(".table-responsive").$(byText("Gender")).parent().lastChild().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().lastChild().shouldHave(text("9980000000"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().lastChild().shouldHave(text("23 June,1995"));
+        $(".table-responsive").$(byText("Subjects")).parent().lastChild().shouldHave(text("English"));
+        $(".table-responsive").$(byText("Hobbies")).parent().lastChild().shouldHave(text("Music"));
+        $(".table-responsive").$(byText("Picture")).parent().lastChild().shouldHave(text("qa_logo.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().lastChild().shouldHave(text("Value"));
+        $(".table-responsive").$(byText("State and City")).parent().lastChild().shouldHave(text("Haryana Karnal"));
     }
 
 }
